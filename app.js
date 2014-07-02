c31b04a8f43986ae91a53aaabf0a96416f966c3e
John Kilpatrick <john@harkable.com>
2014-07-02 08:55:04 +0100

    removed serial

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var lame = require('lame');
 var Speaker = require('speaker');
 var Spotify = require('spotify-web');
-// var uri = process.argv[2] || 'spotify:track:6tdp8sdXrXlPV6AZZN2PE8';
 var uri = process.argv[2] || 'spotify:user:jjkilpatrick:playlist:3a62gxG7RUWIBboQP3RMWv';
 
 var type = Spotify.uriType(uri);
@@ -12,26 +11,9 @@ var password = 'j6206k03';
 
 console.log(type);
 
-// // https://github.com/voodootikigod/node-serialport
-// var SerialPort = require("serialport").SerialPort
-// var serialPort = new SerialPort("/dev/tty-usbserial1", {
-//     baudrate: 9600
-// });
-
 if ('playlist' != type) {
     throw new Error('Must pass a "playlist" URI, got ' + JSON.stringify(type));
 }
-
-// serialPort.on("open", function() {
-//     console.log('open');
-//     serialPort.on('data', function(data) {
-//         console.log('data received: ' + data);
-//     });
-//     serialPort.write("ls\n", function(err, results) {
-//         console.log('err ' + err);
-//         console.log('results ' + results);
-//     });
-// });
 
 Spotify.login(username, password, function(err, spotify) {
     if (err) throw err;
@@ -44,10 +26,6 @@ Spotify.login(username, password, function(err, spotify) {
         playlist.contents.items.forEach(function(track) {
             tracks.push(track.uri);
         });
-
-        // console.log(tracks.map(function(t) {
-        //     return t;
-        // }));
 
         function next() {
             // Get first track from tracks array
